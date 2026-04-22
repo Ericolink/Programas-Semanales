c9fd33c4a685c0bb300f11579c5ea73868b8aa97
ericolink <ericmunoz441@gmail.com>
2026-04-21 22:56:48 -0600

    feat: setup prisma schema and initial migration

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const prisma = require("./prismaClient");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
+});
+
+
+app.get("/members", async (req, res) => {
+  const members = await prisma.member.findMany();
+  res.json(members);
 });
 
 const PORT = 3000;
